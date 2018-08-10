b62f81fb18fd9bb8647211e34e12840f444ff785
Aleksander Gajos <alek.gajos@gmail.com>
2018-08-10 17:26:30 +0200

    Remove coupled counting of lead-trail "hits"

--- Unpacker2/TDCChannel.cc
+++ Unpacker2/TDCChannel.cc
@@ -5,35 +5,22 @@ using namespace std;
 ClassImp(TDCChannel);
 
 TDCChannel::TDCChannel() {
-  hitsNum = 0;
   channel = -1;
 }
 
 TDCChannel::~TDCChannel() {}
 
 
-void TDCChannel::AddHit(double lead, double trail) {
-  leadTimes.push_back(lead);
-  trailTimes.push_back(trail);
-  
-  hitsNum++;
-}
-
 void TDCChannel::AddLead(double lead) {
   leadTimes.push_back(lead);
-  
-  hitsNum++;
 }
 
 void TDCChannel::AddTrail(double trail) {
   trailTimes.push_back(trail);
-  
-  hitsNum++;
 }
 
 void TDCChannel::Clear(Option_t *){
 
-  hitsNum = 0;
   channel = -1;
   
   leadTimes.clear();
