66cad20c2ab66185ef02385f862e049bee86e861
Krzysztof Kacprzak <k.kacprzak@yahoo.com>
2019-05-09 20:48:19 -0300

    making some variables protected instead of private

--- Unpacker2/Unpacker2.h
+++ Unpacker2/Unpacker2.h
@@ -14,42 +14,17 @@
 
 class Unpacker2 : public TObject {
 
-private:
-  std::map<UInt_t, UInt_t> tdc_offsets;
-
-  int eventsToAnalyze;
-
-  bool useTDCcorrection = false;
-
-  size_t reverseHex(size_t n);
-
-  bool areBytesToBeInverted(std::string);
-  bool invertBytes;
-  bool fullSetup;
-
-  bool debugMode;
-
-  long int fileSize;
-
-  int refChannelOffset;
-  TH1F *TOTcalibHist = nullptr;
-  int highest_channel_number = -1;
-  TH1F **TDCcorrections = nullptr;
-
-  const static int kMaxAllowedRepetitions = 1;
-
 public:
   Unpacker2();
   ~Unpacker2() {}
-
   void Init();
   void UnpackSingleStep(const char *hldFile, const char *configFile,
-                        int numberOfEvents, int refChannelOffset,
-                        const char *TOTcalibFile, const char *TDCcalibFile);
+    int numberOfEvents, int refChannelOffset,
+    const char *TOTcalibFile, const char *TDCcalibFile
+  );
 
   TH1F *loadCalibHisto(const char *calibFile);
   bool loadTDCcalibFile(const char *calibFile);
-
   void ParseConfigFile(std::string f, std::string s);
   void DistributeEventsSingleStep(std::string file);
 
@@ -74,17 +49,35 @@ public:
   UInt_t *pHdr;
   UInt_t *subPHdr;
 
-  size_t getSubHdrSize() const { return sizeof(SubEventHdr); }
+  std::string getHubAddress();
   size_t getHdrSize() const { return sizeof(EventHdr); }
+  size_t getSubHdrSize() const { return sizeof(SubEventHdr); }
   UInt_t getFullSize() const { return ((EventHdr *)pHdr)->fullSize; }
   size_t getDataSize();
-  std::string getHubAddress();
   size_t getDataLen() const { return ((getFullSize() - getHdrSize()) + 3) / 4; }
   size_t align8(const size_t i) const { return 8 * size_t((i - 1) / 8 + 1); }
   size_t getPaddedSize() { return align8(getDataSize()); }
   size_t getPaddedEventSize() { return align8(getFullSize()); }
-
   size_t ReverseHex(size_t n);
+
+protected:
+  std::map<UInt_t, UInt_t> tdc_offsets;
+  int highest_channel_number = -1;
+  int refChannelOffset;
+  int eventsToAnalyze;
+  bool debugMode;
+
+private:
+  bool areBytesToBeInverted(std::string);
+  size_t reverseHex(size_t n);
+
+  const static int kMaxAllowedRepetitions = 1;
+  TH1F **TDCcorrections = nullptr;
+  TH1F *TOTcalibHist = nullptr;
+  bool useTDCcorrection = false;
+  long int fileSize;
+  bool invertBytes;
+  bool fullSetup;
 };
 
 #endif
